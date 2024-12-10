c8e7934b163e5d2c5bd52cf40e95f280d0942369
Shepelevvp <shepelevvp@gmail.com>
2024-12-10 22:01:10 +0300

    Initial commit

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
-    @Override
-    public String toString() {
-        return ", в зайце утка " + "";
+    private Duck5 duck;
+
+    public String getRabbit() {
+        return ", в зайце утка " + duck.toString();
+    }
+
+    @Autowired
+    public void setDuck(Duck5 duck) {
+        this.duck = duck;
     }
 }
